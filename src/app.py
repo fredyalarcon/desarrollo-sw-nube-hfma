6ae8e1dbf4d4e32654caa77ce7df57d2d21447b8
Fredy Alarcon <f.alarconf@uniandes.edu.co>
2023-10-15 20:45:35 -0500

    - Se adicionan parametros adicionales al endpoint de la vista task max (int). Parámetro opcional que filtra la cantidad de resultados de una consulta. order (int). Especifica si los resultados se ordenan de forma ascendente (0) o de forma descendente (1) según el ID de la tarea. Ejemplo http://localhost:5000/tasks?order=0&max=3

--- src/app.py
+++ src/app.py
@@ -1,34 +1,22 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_restful import Api
-from flask_jwt_extended import JWTManager, create_access_token
-import jwt
-import datetime
+from flask_jwt_extended import JWTManager
 from modelos import db
 
 from vistas import \
     VistaTasks, VistaTask, \
-    VistaTaskUser
+    VistaTaskUser, VistaGenerarToken
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql@localhost/converter'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['JWT_SECRET_KEY'] = 'frase-secreta'
-#app.config['JWT_ALGORITHM'] = 'HS256'
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 app_context = app.app_context()
 app_context.push()
-
-@app.route('/generar_token')
-def generar_token():
-    # Crea un token con información de usuario y tiempo de expiración
-    expires = datetime.timedelta(minutes=30)
-    access_token = create_access_token(identity='frase-secreta', expires_delta=expires)
-    
-    return jsonify(access_token=access_token)
 
 db.init_app(app)
 ma = Marshmallow(app)
@@ -37,7 +25,8 @@ db.create_all()
 cors = CORS(app)
 
 api = Api(app)
+api.add_resource(VistaGenerarToken, '/generar_token')
 api.add_resource(VistaTasks, '/tasks')
-api.add_resource(VistaTask, '/tasks/<int:id_task>')
+api.add_resource(VistaTask, '/task/<int:id_task>')
 api.add_resource(VistaTaskUser, '/tasks/usuario/<int:usuario_id>')
 jwt = JWTManager(app)
--- src/vistas/vistaTasks.py
+++ src/vistas/vistaTasks.py
@@ -1,24 +1,52 @@
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, create_access_token
 from flask_restful import Resource
 from sqlalchemy import func
+from flask import jsonify, request
+import datetime
 
 from modelos import Task, TaskSchema
 
 task_schema = TaskSchema()
 
-class VistaTasks(Resource):
-    #@jwt_required()
+class VistaGenerarToken(Resource):
     def get(self):
-        tasks = Task.query.all()
+        # Crea un token con información de usuario y tiempo de expiración
+        expires = datetime.timedelta(minutes=30)
+        access_token = create_access_token(identity='frase-secreta', expires_delta=expires)
+        
+        return jsonify(access_token=access_token)
+
+class VistaTasks(Resource):
+    @jwt_required()
+    def get(self):
+        max = request.args.get('max', default=0)
+        order = request.args.get('order', default=0)
+        order = int(order)
+        #ordenar ascendente
+        if order == 0:
+            if max == 0:
+                tasks = Task.query.order_by(Task.id)
+            else:
+                max = int(max)
+                tasks = Task.query.order_by(Task.id).limit(max)
+        else:
+            #ordenar descendente
+            if order ==1:
+                if max == 0:
+                    tasks = Task.query.order_by(Task.id.desc())
+                else:
+                    max = int(max)
+                    tasks = Task.query.order_by(Task.id.desc()).limit(max)
+                    
         return [task_schema.dump(task) for task in tasks]
 
 class VistaTask(Resource):
-    #@jwt_required()
+    @jwt_required()
     def get(self, id_task):
         return task_schema.dump(Task.query.get_or_404(id_task))
     
 class VistaTaskUser(Resource):
-    #@jwt_required()
+    @jwt_required()
     def get(self, usuario_id):
         tasks = Task.query.filter_by(usuario_id=str(usuario_id)).order_by(func.lower(Task.created_at).asc()).all()
         return [task_schema.dump(task) for task in tasks]
