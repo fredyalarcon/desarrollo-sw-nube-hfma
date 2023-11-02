3daa57f564f7c5d55ebd5fb010c52d742a560be6
Humberto Maury <h.maury@uniandes.edu.co>
2023-11-02 13:42:02 -0500

    changes needed by gcloud

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,9 @@
 
 CREATE DATABASE 'converter';
 
-GRANT ALL PRIVILEGES ON converter.* TO mysql@'10.128.0.11' IDENTIFIED BY 'mysql';
+GRANT ALL PRIVILEGES ON converter.* TO mysql@'10.128.0.8' IDENTIFIED BY 'mysql';
+
+GRANT ALL PRIVILEGES ON converter.* TO mysql2@'10.128.0.11' IDENTIFIED BY 'mysql';
 
 # RabbitMQ: 
 
@@ -54,12 +56,28 @@ sudo apt-get install rabbitmq-server -y --fix-missing
 ## Rabbit
 sudo systemctl start rabbitmq-server
 
-# nging
+# nginx
 
 sudo systemctl start nginx  
 
-nginx -c $(pwd)/nginx.conf
+sudo nano /etc/nginx/nginx.conf
 
+# events {
+# }
+# http {
+#     client_max_body_size 100M;
+#     server {
+#         listen 80;
+#         location /api/ {
+#             proxy_pass http://127.0.0.1:5000/;
+#         }
+#         location /static/ {
+#             root /var/converter_data/out/;
+#             #  alias /var/converter_data/out;
+#             autoindex off;
+#         }
+#     }
+# }
 
 # API: 
 
@@ -68,11 +86,17 @@ source ../venv/bin/activate
 gunicorn -w 4 -b 0.0.0.0:5000 'api:create_app()'
 
 
+# Worker
+cd desarrollo-sw-nube-hfma/worker
+source ../venv/bin/activate
+flask run &
+
 # NFS SERVER: 
 
 sudo apt-get update
 
 sudo apt install nfs-kernel-server
+
 sudo mkdir /mnt/converter_data
 
 sudo chown nobody:nogroup /mnt/converter_data #no-one is owner
--- worker/__init__.py
+++ worker/__init__.py
@@ -5,7 +5,7 @@ db_host = os.environ.get("DB_HOST") or 'localhost'
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysql@{}:3306/converter'.format(db_host)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@{}:3306/converter'.format(db_host)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['PROPAGATE_EXCEPTIONS'] = True
     return app
