e6a3991907e9b5c8a536b33318b2b514c6816533
Humberto Maury <h.maury@uniandes.edu.co>
2023-11-19 11:21:11 -0500

    implementacion de pub sub

--- worker/app.py
+++ worker/app.py
@@ -144,9 +144,9 @@ flow_control = pubsub_v1.types.FlowControl(max_messages=1)
 subscriber = pubsub_v1.SubscriberClient()
 # The `subscription_path` method creates a fully qualified identifier
 # in the form `projects/{project_id}/subscriptions/{subscription_id}`
-subscription_path = subscriber.subscription_path(project_id, subscription_id, flow_control=flow_control)
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback, flow_control=flow_control)
 print(f"Listening for messages on {subscription_path}..\n")
 
 # Wrap subscriber in a 'with' block to automatically call close() when done.
